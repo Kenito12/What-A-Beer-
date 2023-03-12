d009acbfc5c1d8dfec28b0baac87ea2240f43311
Ken <nontsuke.ken@gmail.com>
2023-03-11 21:20:40 -0500

    option page working

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,29 @@
+//Pages
+const homePage = document.getElementById("homePage");
+const mainPage = document.getElementById("mainPage");
+const OptionPage = document.getElementById("OptionPage");
+const RandomPage = document.getElementById("RandomPage");
+const WheelPage = document.getElementById("WheelPage");
+
+// Buttons
+const letgoBut = document.getElementById("letgoBut");
+const gtrdm = document.getElementById("gtrdm");
+const gtdg = document.getElementById("gtdg");
+
+
+//Set display default for page 
+mainPage.style.display = "none";
+OptionPage.style.display = "none";
+RandomPage.style.display = "block";
+WheelPage.style.display = "none";
+
 
 // Using JQUERY to handle page load with a delay because page is not heavy enough.
 $(window).on("load",function(){
-    $(".loader-wrapper").delay(1200).fadeToggle(300);
-    // $(".loader-wrapper").hide();
+    // $(".loader-wrapper").delay(1200).fadeToggle(300);
+    $(".loader-wrapper").hide();
 });
-
-const homePage = document.getElementById("homePage");
-const op = document.getElementById("opening");
-const letgoBut = document.getElementById("letgoBut");
+//----------------------------------------
 
 
 //bodymovin js to play Lottiefiles
@@ -19,54 +35,50 @@ const animItem = bodymovin.loadAnimation({
     autoplay: true,
     path:'https://assets10.lottiefiles.com/packages/lf20_gB20zF.json'
 });
+//----------------------------------------
 
 //Animejs animation Mainpage
-
-var animation = anime({
+var mainpageLoad = anime({
     targets: '.op', 
     delay: 1300,
     translateY : 300,
+    autoplay: false
   });
 
-var chooseAnime = anime({
+var chooseLoad = anime({
     targets: '.op', 
-    // delay: 100,
     translateY : 120,
     autoplay: false
   });
 
-// Set Delay on homepage element to change the index after 3 sec
+// Set Delay on homepage element to change the index after 2 sec
 setTimeout('homePage.style.zIndex = "1";', 2000);
-
-function Letgo (){
-
-
-
-    var choose = `<h1 class="options">Choose your features!</h1>
-    <div class="opWrapper">
-        <div class="randomizer">
-            <div class="wrapChild">
-                <img class="iconImg" src="IMG/RandomizerIcon.png" alt="Radomizer_ICON">
-                <h2 class="opTitle">Randomizer</h2>
-            </div>
-        </div>
-        <div class="drinkingGame">
-            <div class="wrapChild">
-                <img class="iconImg" src="IMG/DrinkingGameIcon.png" alt="DrinkingGame_ICON">
-                <h2 class="opTitle">Drinking Game</h2>
-            </div>
-        </div>
-    </div>`
-    op.innerHTML = ""//clean the element
-    op.innerHTML += choose;
-}
+//----------------------------------------
 
 
 //onclick even for Lets Go button after release play new animation
 letgoBut.onclick = function(){
-    Letgo();
+    mainPage.style.display = "none";
+    OptionPage.style.display = "block";
     console.log("working!!")
 };
+letgoBut.onmouseup = chooseLoad.play;
+//----------------------------------------
 
-letgoBut.onmouseup = chooseAnime.play;
-// ----------------------
+//onclick event to go to Randomizer after button release play new animation
+gtrdm.onclick = function(){
+    OptionPage.style.display = "none";
+    RandomPage.style.display = "block";
+    console.log("working!!")
+};
+gtrdm.onmouseup = chooseLoad.play;
+//----------------------------------------
+
+//onclick event to go to Drinking game after button release play new animation
+gtdg.onclick = function(){
+    OptionPage.style.display = "none";
+    WheelPage.style.display = "block";
+    console.log("working!!")
+};
+gtdg.onmouseup = chooseLoad.play;
+//----------------------------------------
